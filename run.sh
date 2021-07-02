4b6666e2b2ff59778712b35d4d7567934542a763
twopin <twopinlei@hotmail.com>
2021-07-02 17:53:17 +0800

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,16 @@
+# this shell script provides an easy way to apply CAMP on deciphering peptide-protein interactions
+# make sure that pip can work on your server
+
+
+pip install Keras==2.0.8
+pip install tensorflow=1.2.1
+
+unzip example_data_feature.zip
+unzip ./model/CAMP.h5.zip
+unzip ./model/CAMP_BS.h5.zip
+
+mkdir example_prediction
+
+python -u predict_CAMP.py
+
+
