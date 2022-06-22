e723fe6d1c82a52a9721915ede68da0d7fbfec9d
twopin <twopinlei@hotmail.com>
2022-06-22 14:36:55 +0800

    Add files via upload

---
+++ data_prepare/download_inter.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 12 13:43:12 2020
+
+@author: lenovo
+"""
+
+import urllib
+import csv
+csvfile=open('pepBDB_results.csv','r')
+reader=csv.reader(csvfile)
+querys={}
+for item in reader:
+            if reader.line_num==1:
+                continue
+            pdbid=item[0][:6]
+            url = 'http://huanglab.phys.hust.edu.cn/pepbdb/db/'+pdbid+'/inter.dat'
+            urllib.request.urlretrieve(url, pdbid+'.dat')
+print('down')
